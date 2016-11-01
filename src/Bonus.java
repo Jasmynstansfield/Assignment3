20d8e3f92817dd4265dc1904ea43bf97eb30d5d7
Jazz <jasmynpaige14@gmail.com>
2016-11-01 10:53:50 -0500

    Assignment 3
    Lab for Mr. Hardmans class

---
+++ src/Bonus.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * Jasmyn Stansfield
+ * Mr. Hardman
+ * Assignment 3, bonus
+ * November 1, 2016
+ */
+public class Bonus {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner userInput = new Scanner(System.in);
+		
+		int userNum;
+		int randomNum;
+		
+		
+		
+		do {
+			
+			Random randGenerator = new Random();
+			randomNum = randGenerator.nextInt(9) + 1;
+			
+			System.out.println("Guess a number between 1 and 10");
+			userNum = userInput.nextInt();
+		
+			if (userNum != randomNum) {
+			
+				System.out.println("Sorry, the correct number is " + randomNum + ". ");
+			}
+		
+			else if (userNum == -1){
+				System.out.println("I'm sad to see you go :(");
+				userInput.close();
+			}
+			else {
+				System.out.println("Corect, good job!");
+				userInput.close();
+			}
+		
+		} while (userNum != randomNum);
+	}
+
+}
